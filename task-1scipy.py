c7c9e7741be3bfacf7cc477a0bf5eccfac7f1a06
tusharnandy <55907266+tusharnandy@users.noreply.github.com>
2020-04-15 15:58:17 +0530

    Update task-1scipy.py

--- task-1scipy.py
+++ task-1scipy.py
@@ -2,6 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy import stats
+from sklearn import metrics
 
 path = "Admission_Predict.csv"
 df = pd.read_csv(path)
@@ -9,6 +10,8 @@ df = pd.read_csv(path)
 y = np.array(df['ChanceofAdmit'])
 x = np.array(df['CGPA'])
 
+y_end = y[390:]
+print(len(x))
 slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
 
 y_fit = slope*x + intercept
@@ -16,8 +19,9 @@ std_err *= 10
 y_max= y_fit + std_err
 y_min = y_fit - std_err
 
+y_fit_end = y_fit[390:]
+
 plt.scatter(x, y, color='yellow', label='original data')
-plt.scatter((0.8 - intercept)/slope, 0.8, color='blue')
 
 plt.plot(x, y_fit, color='red', label='curve fit')
 plt.plot(x, y_max, color='blue', label='curve limit')
@@ -27,7 +31,6 @@ plt.xlabel('CGPA')
 plt.ylabel("Chances")
 plt.legend()
 plt.show()
-print(std_err)
 
 y = y*5
 
@@ -37,3 +40,10 @@ extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 plt.clf()
 plt.imshow(heatmap.T, extent=extent, origin='lower')
 plt.show()
+
+total_MS = metrics.mean_squared_error(y, y_fit)
+print(total_MS)
+print(f"RMS error of the fit is: {np.sqrt(total_MS)}")
+
+end_ms = metrics.mean_squared_error(y_end, y_fit_end)
+print(f"rms error for the last 10 values: {np.sqrt(end_ms)}")
