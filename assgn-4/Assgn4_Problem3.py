ac59442b15af1db4318a1f3fae5a901d76451649
tusharnandy <55907266+tusharnandy@users.noreply.github.com>
2020-04-16 17:57:23 +0530

    Add files via upload

---
+++ assgn-4/Assgn4_Problem3.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as seabornInstance
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+
+
+df = pd.read_csv("Admission_Predict_LA.csv")
+df.drop(columns=['Serial No.'], inplace=True)
+headers = list(df.columns)
+target = headers.pop()
+X = df[headers].values
+y = df[target].values
+
+
+
+plt.figure(figsize=(15,10))
+plt.tight_layout()
+seabornInstance.distplot(df[target])
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+
+
+coeff_df = pd.DataFrame(regressor.coef_, headers, columns=['Coefficient'])
+print(coeff_df)
+
+
+
+y_pred = regressor.predict(X_test)
+
+
+
+final = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+print(final.head(25))
+
+
+
+MAE = metrics.mean_absolute_error(y_test, y_pred)
+RMSE = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+print(f"Mean Absolute Error: {MAE}")
+print(f"Root Mean Squared Error: {RMSE}")
+print(np.mean(y))
+print(f"Error % is {RMSE/(np.mean(y)) * 100}")
